df0fed3c1aa8c1370189ef75c78f24175ab4c3ad
disagon01 <100069014+disagon01@users.noreply.github.com>
2024-09-06 21:48:38 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.viinas.eu.org": dockerHub,
+  "quay.viinas.eu.org": "https://quay.io",
+  "gcr.viinas.eu.org": "https://gcr.io",
+  "k8s-gcr.viinas.eu.org": "https://k8s.gcr.io",
+  "k8s.viinas.eu.org": "https://registry.k8s.io",
+  "ghcr.viinas.eu.org": "https://ghcr.io",
+  "cloudsmith.viinas.eu.org": "https://docker.cloudsmith.io",
+  "ecr.viinas.eu.org": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.viinas.eu.org": dockerHub,
 };
 
 function routeByHosts(host) {
